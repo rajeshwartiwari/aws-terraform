8b35c40b266ee56219d2df617dd76e582286672d
Rajeshwar Tiwari <rajeshwar.tiwari@tcs.com>
2020-05-21 14:48:18 +0100

    second commit

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_iam_instance_profile" "kms_profile" {
   role = "EC2KMSRole"
 }
 
-resource "aws_instance" "publicInstance" {
+resource "aws_instance" "publicInstance1" {
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
   subnet_id = "${aws_subnet.subnet_public.id}"
@@ -13,8 +13,24 @@ resource "aws_instance" "publicInstance" {
 
   tags ={
 		"Environment" = "${var.environment_tag}"
+    "Name" = "publicInstance1"
 	}
 }
+
+resource "aws_instance" "publicInstance2" {
+  ami           = "${var.instance_ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${aws_subnet.subnet_public.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
+  vpc_security_group_ids = ["${aws_security_group.sgpublic.id}"]
+  key_name = "lbgamwlinux"
+
+  tags ={
+		"Environment" = "${var.environment_tag}"
+    "Name" = "publicInstance2"
+	}
+}
+
 
 resource "aws_instance" "privateInstance" {
   ami           = "${var.instance_ami}"
--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 provider "aws" {
   version = "~> 2.0"
-  #access_key = var.access_key 
-  #secret_key = var.secret_key 
   region     = var.region
-  #shared_credentials_file = "C:\\Users\\Rajesh\\.aws\\credentials"
-  #profile                 = "customprofile"
 }
