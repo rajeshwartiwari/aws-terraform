8a26d3def6880e5ca43d7485cb891dcaa96c540e
Rajeshwar Tiwari <rajeshwar.tiwari@tcs.com>
2020-05-22 11:58:00 +0100

    Bakend configuration

--- main.tf
+++ main.tf
@@ -2,3 +2,21 @@ provider "aws" {
   version = "~> 2.0"
   region     = var.region
 }
+
+
+
+ terraform {
+   backend "s3" {
+     bucket = "cp-terraform-bootstrap-terraform-state"
+     key    = "default-infrastructure"
+     region = "eu-west-2"
+   }
+ }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "cp-terraform-bootstrap-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+}
