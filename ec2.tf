608bab4a13b187848afab384b1d2d10d8c030ae2
Rajeshwar Tiwari <rajeshwar.tiwari@tcs.com>
2020-05-22 17:35:14 +0100

    commited change

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,14 @@
 
+resource "aws_iam_instance_profile" "kms_profile" {
+  name = "kms_profile"
+  role = "EC2KMSRole"
+}
+
 resource "aws_instance" "publicInstance1" {
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
   subnet_id = "${aws_subnet.subnet_public.id}"
-  #iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
+  iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
   vpc_security_group_ids = ["${aws_security_group.sgpublic.id}"]
   key_name = "lbgamwlinux"
 
@@ -32,7 +37,7 @@ resource "aws_instance" "privateInstance" {
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
   subnet_id = "${aws_subnet.subnet_private.id}"
-  #iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
+  iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
   vpc_security_group_ids = ["${aws_security_group.sgprivate.id}"]
   key_name = "lbgamwlinux"
 
