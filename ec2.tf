35ab3aa9c871714d3887083810e3f4ed82dda36d
Rajeshwar Tiwari <rajeshwar.tiwari@tcs.com>
2020-06-03 18:36:43 +0100

    added window image

--- ec2.tf
+++ ec2.tf
@@ -18,17 +18,17 @@ resource "aws_instance" "publicInstance1" {
 	}
 }
 
-rource "aws_instance" "publicInstance2" {
-ami           = "${var.instance_ami1}"
-instance_type = "${var.instance_type1}"
-subnet_id = "${aws_subnet.subnet_public.id}"
-iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
-vpc_security_group_ids = ["${aws_security_group.sgpublic.id}"]
-key_name = "lbgamwlinux"
+resource "aws_instance" "publicInstance2" {
+  ami           = "${var.instance_ami1}"
+  instance_type = "${var.instance_type1}"
+  subnet_id = "${aws_subnet.subnet_public.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.kms_profile.name}"
+  vpc_security_group_ids = ["${aws_security_group.sgpublic.id}"]
+  key_name = "lbgamwlinux"
 
-ags ={
-		"Environment" = "${var.environment_tag}"
-    "Name" = "publicInstance2"
+  tags ={
+      "Environment" = "${var.environment_tag}"
+      "Name" = "publicInstance2"
 	}
 
 
